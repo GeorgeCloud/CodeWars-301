f65ecbd1f32ea011096f9e5b5f1af6052e8f098c
George Ceja <net.george.sky@gmail.com>
2017-11-27 12:12:05 -0800

    Update replaceDots.js

--- replaceDots.js
+++ replaceDots.js
@@ -1,4 +1,5 @@
 // November 27, 2017
+// Problem ------> https://www.codewars.com/kata/fixme-replace-all-dots
 
 // Splits all '.' from string parameter and sets it equal to '-' if present
 function replaceDots(str){
