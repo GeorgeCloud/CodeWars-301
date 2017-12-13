a77d3f3a2ba124587009d8a9797d20905c64ace9
George Ceja <net.george.sky@gmail.com>
2017-12-13 11:53:37 -0800

    Update solution.js

--- sum-of-odd-cubed-elements/solution.js
+++ sum-of-odd-cubed-elements/solution.js
@@ -2,7 +2,7 @@ function cubeOdd(arr) {
   sum = 0;
   for(i in arr){
     if(isNaN(arr[i])){return undefined;}
-    if(arr[i]%2==1){sum += arr[i] ** 3}
+    if(arr[i]%2==1 || arr[i]%2==-1 ){sum += arr[i] * arr[i] * arr[i]}
   }
   return sum;
 }
